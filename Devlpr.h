90e178b020fa59031528376a1654a17c6e298e05
Finn Kuusisto <finn.kuusisto@gmail.com>
2021-05-10 14:47:09 -0500

    Functional filter but not actually working because it's way too slow for 1kHz sampling rate as yet.

--- Devlpr.h
+++ Devlpr.h
@@ -9,17 +9,22 @@ class Devlpr
         Devlpr();
         void tick();
         int lastValue();
+        float lastValueFiltered();
+        float windowAvg();
+        float dummy;
     private:
         // general buffer bookkeeping
-        static const byte BUFSIZE = 129;
+        static const byte BUFSIZE = 155; // size of filter
         byte bufInd;
         int buf[BUFSIZE];
         unsigned long lastTickMicros = 0;
         // emg tracking
         unsigned long MICROS_SCHED_EMG = 1000;
-        void readEMG();
-        int emgPin;
         unsigned long microsSinceEMG;
+        unsigned long emgRunningSum;
+        int emgPin;
+        int emgVal;
+        void readEMG();
 };
 
 #endif
