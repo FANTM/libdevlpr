2c659a29a42f681e1c8031f685753f65ce56e994
Finn Kuusisto <finn.kuusisto@gmail.com>
2021-06-23 13:29:10 -0500

    Adding/moving things to meet the Arduino library specification.

---
+++ examples/plot_peak_to_peak/plot_peak_to_peak.ino
@@ -0,0 +1,26 @@
+#include "Devlpr.h"
+
+Devlpr devlpr;
+
+void printEMG(Devlpr *d) {
+    int result = d->windowPeakToPeakAmplitude();
+    Serial.print("0 1024 ");
+    Serial.println(result);
+}
+
+void writeFlex(Devlpr *d) {
+    Serial.println("FLEX");
+}
+
+void setup() {
+    Serial.begin(2000000);
+    // add our print function to our DEVLPR schedule
+    // try to run once every 1ms
+    devlpr.scheduleFunction(printEMG, 1);
+    //devlpr.setFlexCallback(writeFlex);
+}
+
+void loop() {
+    // let the DEVLPR library do its job
+    devlpr.tick();
+}
---
+++ src/libdevlpr.h
@@ -0,0 +1,8 @@
+#ifndef libdevlpr_h
+#define libdevlpr_h
+
+#define LIBDEVLPR_VERSION "0.1.1"
+
+#include "Devlpr.h"
+
+#endif
